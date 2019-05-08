575ec4df4e904d4d0570d5e4b047f08fbbc39f60
StephenDumore <48033348+StephenDumore@users.noreply.github.com>
2019-05-08 00:24:32 -0700

    First Commit
    12:23 AM 5/8/19

---
+++ client_project/index.php
@@ -0,0 +1,72 @@
+<?php
+//echo 'hello';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amiri" rel="stylesheet">
+</head>
+
+<body>
+    <div class="home-parallax">
+        <h1 class="home-title">CHS Book Club</h1>
+        <div class="title-btn-padding">
+            <button class="title-btn">Scroll Down</button>
+        </div>
+    </div>
+    <div class="mission">
+        <h2 class="title">Mission</h2>
+        <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum recusandae, voluptatem magni quod molestiae tenetur iste voluptatibus! Voluptas, inventore explicabo vel velit repudiandae. Atque ullam, saepe cupiditate. Reiciendis eaque, fugiat.</p>
+    </div>
+
+    <div class="img-a">
+        <a href="#">Schedule</a>
+        <a href="#">Join</a>
+        <a href="#">More Information</a>
+    </div>
+
+    <h2 class="title">Calendar</h2>
+    <div class="calendar">
+        <div class="Month1">
+            <p class="calendar-month">April</p>
+            <ul>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+            </ul>
+        </div>
+        <div class="Month2">
+            <p class="calendar-month">May</p>
+            <ul>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+            </ul>
+        </div>
+        <div class="Month3">
+            <p class="calendar-month">July</p>
+            <ul>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+                <li>Lorem ispsum dolor Sit Ammet</li>
+            </ul>
+        </div>
+    </div>
+    <div class="parallax-bottom"></div>
+
+</body>
+
+</html>
