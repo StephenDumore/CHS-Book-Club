0ab082d68fefdca4463336eb054f8b608262a472
StephenDumore <48033348+StephenDumore@users.noreply.github.com>
2019-05-15 02:12:58 -0700

    5.15.19 completed light box

--- aboutus.php
+++ aboutus.php
@@ -31,17 +31,25 @@
         </div>
     </div>
     <div class="lightbox">
-        <div class="lightbox-img"><img class="img-min-width" src="img/turtles.jpg" onclick="display(); currentSlide(1)" alt="A copy of John Green's novel, Turtles all the way down"></div>
-        <div class="lightbox-img"><img class="img-min-width" src="img/group.jpg" onclick="display(); currentSlide(2)" alt="The CHS book club working on posters"></div>
-        <div class="lightbox-img"><img class="img-min-width" src="img/classroom.jpg" onclick="display(); currentSlide(3)" alt="The CHS book club's classroom"></div>
-        <div class="lightbox-img"><img class="img-min-width" src="img/bookshelf-thumnail.jpg" onclick="display(); currentSlide(4)" alt="A bookshelf in the CHS book club classroom"></div>
+        <h2 class="lightbox-title">Click an image to enlarge</h2>
+        <div class="lightbox-imgs">
+            <div class="lightbox-img">
+                <div class="fade" ></div>
+                <div class="lb-fade-text" onclick="display(); currentSlide(1)">
+                    <p>+</p>
+                </div><img class="img-min-width" src="img/turtles.jpg" alt="A copy of John Green's novel, Turtles all the way down">
+            </div>
+            <div class="lightbox-img"><img class="img-min-width" src="img/group.jpg" onclick="display(); currentSlide(2)" alt="The CHS book club working on posters"></div>
+            <div class="lightbox-img"><img class="img-min-width" src="img/classroom.jpg" onclick="display(); currentSlide(3)" alt="The CHS book club's classroom"></div>
+            <div class="lightbox-img"><img class="img-min-width" src="img/bookshelf-thumnail.jpg" onclick="display(); currentSlide(4)" alt="A bookshelf in the CHS book club classroom"></div>
+        </div>
     </div>
 
     <div id="lightboxDisplayed" class="lightbox-displayed">
         <span class="closeButton" id="closeButton" onclick="closeThing()">X</span>
         <a class="prev" onclick="nextSlide(-1)">i</a>
-        <a class="next" onclick="nextSlide(1)">i</a>
-        
+        <a class="next" onclick="nextSlide(1)">j</a>
+
         <div class="lightbox-img-d"><img class="img-min-width" src="img/turtles.jpg" alt="A copy of John Green's novel, Turtles all the way down"></div>
         <div class="lightbox-img-d"><img class="img-min-width" src="img/group.jpg" alt="The CHS book club working on posters"></div>
         <div class="lightbox-img-d"><img class="img-min-width" src="img/classroom.jpg" alt="The CHS book club's classroom"></div>
@@ -81,11 +89,15 @@
         function openSlide(n) {
             var i;
             var slides = document.getElementsByClassName("lightbox-img-d");
-            if (n > slides.length) {indexSlides = 1}
-            if (n < 1) {indexSlides = slides.length;}
-            
+            if (n > slides.length) {
+                indexSlides = 1
+            }
+            if (n < 1) {
+                indexSlides = slides.length;
+            }
+
             for (i = 0; i < slides.length; i++) {
-                slides[i].style.display ="none";
+                slides[i].style.display = "none";
             }
             slides[indexSlides - 1].style.display = "block";
         }
