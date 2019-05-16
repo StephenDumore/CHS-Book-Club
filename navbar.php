a2b0ef52a8e8316d4c1f81961df2b88174f45b4f
StephenDumore <48033348+StephenDumore@users.noreply.github.com>
2019-05-16 02:59:47 -0700

    completed most of everything

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amiri" rel="stylesheet">
+
+</head>
+    <div id="nav" class="navbar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="aboutus.php">About Us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <div class="navbar-line"></div>
+</div>
+
+</html>
