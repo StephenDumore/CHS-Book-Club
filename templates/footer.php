e3999f74985ac3d7b1255f1bfd5310d83bcb7dd3
StephenDumore <stephen.dumore@student.cart.org>
2019-05-16 22:43:28 -0700

    added templates

---
+++ templates/footer.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+   <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    
+</head>
+<footer>
+    <button onclick="topBtn()">Top</button>
+    <div class="line"></div>
+    <div class="footer-bottom">
+        <div class="footer-info">
+            <p>Room: P13</p>
+            <p>1055 Fowler Ave, Clovis, CA 93611</p>
+            <p>Club advisor: LeanneLewis@clovisusd.k12.ca.us</p>
+        </div>
+        <div class="footer-icons">
+            <a href="https://www.instagram.com/chs.book.club/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="LeanneLewis@clovisusd.k12.ca.us" target="_blank"><i class="far fa-envelope"></i></a>
+        </div>
+    </div>
+</footer>
+<script>
+    function topBtn() {
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+    }
+    
+</script>
+
+</html>
+
---
+++ templates/navbar.php
@@ -0,0 +1,13 @@
+<?php
+?>
+
+<nav>
+    <div id="nav" class="navbar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="aboutus.php">About Us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <div class="navbar-line"></div>
+    </div>
+</nav>
